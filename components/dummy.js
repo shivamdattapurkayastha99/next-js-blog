fb08e80561e62b33bf9cdfebef91391c0a4d5110
Shivam Datta  Purkayastha <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2022-02-22 21:06:10 +0530

    Add files via upload

---
+++ components/dummy.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+      <>
+      <style jsx >
+          {`
+          .dummy{
+              background:yellow;
+          }
+          `}
+      </style>
+      <div className='dummy'>Dummy</div>
+      </>
+    
+  )
+}
+
+export default Dummy
