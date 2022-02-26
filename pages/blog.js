3c4e1f93d8e14c5123523f74534ba02c64d62cec
Shivam Datta  Purkayastha <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2022-02-26 20:53:27 +0530

    Add files via upload

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import styles from '../styles/Blog.module.css' 
+import Link from 'next/link'
 const Blog = () => {
   return (
-    <div>this is bloghome</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+          
+            <div className={styles.blogItem}>
+            <Link href={'/blogpost/learn-javascript'}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>Javascript is the language used to design logic for the web </p>
+            </div>
+            <div className="blogItem">
+            <Link href={'/blogpost/learn-javascript'}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>Javascript is the language used to design logic for the web </p>
+            </div>
+            <div className="blogItem">
+            <Link href={'/blogpost/learn-javascript'}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>Javascript is the language used to design logic for the web </p>
+            </div>
+            </main>
+        </div>
   )
 }
 
